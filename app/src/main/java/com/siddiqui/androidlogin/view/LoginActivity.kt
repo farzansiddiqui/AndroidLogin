9c838b6ed27f7f892c2b27c79c8ad2f322f45b3d
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-07-17 20:53:20 +0530

    upload file on github..

---
+++ app/src/main/java/com/siddiqui/androidlogin/view/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.siddiqui.androidlogin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.siddiqui.androidlogin.R
+import com.siddiqui.androidlogin.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
